91611b4f3df4ef1adb41d91eee135397b6c6ba64
shristi482004 <shristish484@gmail.com>
2026-03-19 13:45:53 +0530

    added error detection and management

--- app/api/upload_files.py
+++ app/api/upload_files.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, HTTPException
 from app.services.dataset_services import analyze_dataset
 import pandas as pd
 
@@ -6,8 +6,18 @@ router=APIRouter()
 
 @router.post("/upload")
 async def upload_dataset(file: UploadFile = File(...)):
-    df=pd.read_csv(file.file)
-    
-    result=analyze_dataset(df)
 
-    return result
+    if not file.filename.endswith('.csv'):
+        raise HTTPException(status_code=400, detail="Only CSV files are allowed")
+    
+    try:
+        df=pd.read_csv(file.file)
+
+        if df.empty():
+            raise HTTPException(status_code=400, detail="Uploaded CSV file is empty")
+        
+        result=analyze_dataset(df)
+        return result
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail='Error processing the uploaded file: ' + str(e))
